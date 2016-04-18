5b65cd93bc02df0167c42e0124b84283e7cd95c3
Steven Pribilinskiy <steven.pribilinskiy@gmail.com>
2016-04-18 18:20:21 +0300

    Add `_.gpIndexOfNonStrict` and `_.gpIncludesNonStrict`

---
+++ array/gpIndexOfNonStrict.js
@@ -0,0 +1,43 @@
+(function() {
+
+	_.mixin({gpIndexOfNonStrict: gpIndexOfNonStrict});
+
+	/**
+	 * Same as `_.indexOf` except that non-strict equality operator is used for comparison
+	 *
+	 * @see {@link _.indexOf}
+	 * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Comparison_Operators#Equality_()}
+	 *
+	 * @example
+	 *
+	 * _.indexOf([1, 2, 1, 2], '2');
+	 * // => 1
+	 */
+	function gpIndexOfNonStrict(array, value, fromIndex) {
+
+		var length = array ? array.length : 0;
+
+		if (!length) {
+			return -1;
+		}
+
+		fromIndex = _.toInteger(fromIndex);
+
+		if (fromIndex < 0) {
+			fromIndex = _.max(length + fromIndex, 0);
+		}
+
+		var index = fromIndex - 1;
+
+		while (++index < length) {
+
+			if (array[index] == value) {
+				return index;
+			}
+		}
+
+		return -1;
+	}
+
+})();
+
---
+++ collection/gpIncludesNonStrict.js
@@ -0,0 +1,49 @@
+(function() {
+
+	_.mixin({gpIncludesNonStrict: gpIncludesNonStrict});
+
+	/**
+	 * Same as `_.includes` except that
+	 * non-strict equality comparison operator is used for comparison
+	 *
+	 * @see {@link _.includes}
+	 * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Comparison_Operators#Equality_()}
+	 *
+	 * @example
+	 *
+	 * _.includes([1, 2, 3], '1');
+	 * // => true
+	 *
+	 * _.includes({ 'user': 'fred', 'age': '40' }, 40);
+	 * // => true
+	 *
+	 * _.includes([undefined], null);
+	 * // => true
+	 *
+	 * _.includes([1], true);
+	 * // => true
+	 * 
+	 * _.includes('', false);
+	 * // => true
+	 */
+	function gpIncludesNonStrict(collection, value, fromIndex, guard) {
+
+		collection = _.isArrayLike(collection) ? collection : _.values(collection);
+		fromIndex = (fromIndex && !guard) ? _.toInteger(fromIndex) : 0;
+
+		var length = collection.length;
+		if (fromIndex < 0) {
+			fromIndex = _.max(length + fromIndex, 0);
+		}
+
+		if (_.isString(collection) && collection) {
+			return (fromIndex <= length && collection.indexOf(value, fromIndex) > -1);
+		} else if (_.isArray()) {
+			return (!!length && baseIndexOf(collection, value, fromIndex) > -1);
+		} else {
+			return collection == value;
+		}
+	}
+
+})();
+
